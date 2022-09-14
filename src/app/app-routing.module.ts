213e69dd14ed130a9e42b2215730b2eef909fce3
prabhat <prabhatproject1@gmail.com>
2022-09-14 22:47:13 +0530

    making auth component as a default page..running

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
 import { DogTableComponent } from './dog-table/dog-table.component';
 
-const routes: Routes = [{ path: "listdog", component: DogTableComponent }];
+const routes: Routes =
+ [
+  { path: "listdog", component: DogTableComponent },
+  { path: "**", component: AuthComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
